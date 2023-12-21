385fe8c694210a2e853198d73d666ac8f3b6900b
Terence Ponce <terenceponce@gmail.com>
2023-12-21 22:28:43 +0000

    Initial Commit

---
+++ lib/diff_check.ex
@@ -0,0 +1,18 @@
+defmodule DiffCheck do
+  @moduledoc """
+  Documentation for `DiffCheck`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DiffCheck.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/diff_check_test.exs
@@ -0,0 +1,8 @@
+defmodule DiffCheckTest do
+  use ExUnit.Case
+  doctest DiffCheck
+
+  test "greets the world" do
+    assert DiffCheck.hello() == :world
+  end
+end
